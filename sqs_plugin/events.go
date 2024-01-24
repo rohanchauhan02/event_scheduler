31861bd773612030713c67339dc14c62ef59d6c1
rohanchauhan02 <rohan021195@gmail.com>
2024-01-25 00:27:18 +0530

    sql query

--- sqs_plugin/events.go
+++ sqs_plugin/events.go
@@ -9,15 +9,17 @@ import (
 
 // SQSPlugin is a GORM plugin for triggering SQS messages on updates
 type SQSPlugin struct {
-	SQSClient *sqs.SQS
-	MysqlSess *gorm.DB
+	SQSClient  *sqs.SQS
+	MysqlSess  *gorm.DB
+	QueueURI   *string // Replace with your actual SQS Queue URL
+	SQSMessage *string // Replace with your actual SQS message structure
 }
 
-func (p *SQSPlugin) Update(value interface{}) error {
+func (p *SQSPlugin) Update(query string, value interface{}) error {
 	// Register the plugin's callback
 	p.MysqlSess.Callback().Update().Before("gorm:before_update").Register("sqs_plugin:before_update", p.beforeUpdate)
 	p.MysqlSess.Callback().Update().After("gorm:after_update").Register("sqs_plugin:after_update", p.afterUpdate)
-	// p.MysqlSess.Updates(value)
+	p.MysqlSess.Where(query).Updates(value)
 	return nil
 }
 
@@ -29,15 +31,15 @@ func (p *SQSPlugin) beforeUpdate(db *gorm.DB) {
 
 // afterUpdate is the callback function to be executed after an update operation
 func (p *SQSPlugin) afterUpdate(db *gorm.DB) {
-	fmt.Println("After update callback triggered. Query......")
-	// Perform your SQS message pushing logic here
-	// _, err := p.SQSClient.SendMessage(&sqs.SendMessageInput{
-	// 	MessageBody: p.SQSMessage,
-	// 	QueueUrl:    p.QueueURI,
-	// })
-	// if err != nil {
-	// 	fmt.Println("Failed to published message to sqs")
-	// 	return
-	// }
+	fmt.Println("After update callback triggered. Pushing message to SQS...")
+	_, err := p.SQSClient.SendMessage(&sqs.SendMessageInput{
+		MessageBody: p.SQSMessage,
+		QueueUrl:    p.QueueURI,
+	})
+	if err != nil {
+		fmt.Println("Failed to publish message to SQS:", err)
+		return
+	}
+	fmt.Println("Message successfully published to SQS.")
 	fmt.Println(db.Statement.SQL.String())
 }
