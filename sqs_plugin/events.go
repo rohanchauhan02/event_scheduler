af3c2cdc586d66c4999366437fb569b72064ed55
rohanchauhan02 <rohan021195@gmail.com>
2024-01-25 02:42:27 +0530

    update overall functionality

--- sqs_plugin/events.go
+++ sqs_plugin/events.go
@@ -1,7 +1,10 @@
 package sqs_plugin
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"gorm.io/gorm"
@@ -9,37 +12,111 @@ import (
 
 // SQSPlugin is a GORM plugin for triggering SQS messages on updates
 type SQSPlugin struct {
-	SQSClient  *sqs.SQS
-	MysqlSess  *gorm.DB
-	QueueURI   *string // Replace with your actual SQS Queue URL
-	SQSMessage *string // Replace with your actual SQS message structure
+	mu           sync.Mutex
+	SQSClient    *sqs.SQS
+	MysqlSess    *gorm.DB
+	QueueURI     *string // Replace with your actual SQS Queue URL
+	SQSMessage   *string // Replace with your actual SQS message structure
+	OldData      interface{}
+	NewData      interface{}
+	TriggerEvent bool
 }
 
-func (p *SQSPlugin) Update(query string, value interface{}) error {
+func (p *SQSPlugin) Update(value interface{}, column string, query string, args ...interface{}) error {
+	// Use a mutex to ensure exclusive access to the database
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	// Register the plugin's callback
-	p.MysqlSess.Callback().Update().Before("gorm:before_update").Register("sqs_plugin:before_update", p.beforeUpdate)
-	p.MysqlSess.Callback().Update().After("gorm:after_update").Register("sqs_plugin:after_update", p.afterUpdate)
-	p.MysqlSess.Where(query).Updates(value)
+	db := p.MysqlSess
+	db.Callback().Update().Before("gorm:before_update").Register("before_update", func(d *gorm.DB) { p.beforeUpdate(d, column) })
+	db.Callback().Update().After("gorm:after_update").Register("after_update", p.afterUpdate)
+
+	// Perform the update
+	if err := db.Where(query, args...).Updates(value).Error; err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // beforeUpdate is the callback function to be executed before an update operation
-func (p *SQSPlugin) beforeUpdate(db *gorm.DB) {
-	fmt.Println("before update callback triggered. Pushing message to SQS...")
-	fmt.Println(db.Statement.SQL.String())
+func (p *SQSPlugin) beforeUpdate(db *gorm.DB, columnName string) {
+	fmt.Println("Before update callback triggered. Finding existing data...")
+
+	// Debugging: Print the SQL query before execution
+	db = db.Debug()
+
+	// Query and store the existing data
+	if err := db.First(p.OldData).Error; err != nil {
+		// Handle the error if necessary
+		fmt.Println("Error finding existing data:", err)
+		return
+	} else {
+		fmt.Println("Existing data found:", p.OldData)
+	}
+
+	updatedData := db.Statement.Dest
+	fmt.Println("Updated data:", updatedData)
+	jsonData, err := json.Marshal(updatedData)
+	if err != nil {
+		fmt.Println("Error marshaling data to JSON:", err)
+		return
+	}
+
+	if err := json.Unmarshal(jsonData, &p.NewData); err != nil {
+		fmt.Println("Error unmarshaling JSON to NewData:", err)
+		return
+	}
+
+	// Use reflection to dynamically access the field value based on the provided column name
+	oldValueField := reflect.ValueOf(p.OldData).Elem().FieldByName(columnName)
+	newValueField := reflect.ValueOf(p.NewData).Elem().FieldByName(columnName)
+
+	// Check if the field is valid
+	if !oldValueField.IsValid() || !newValueField.IsValid() {
+		fmt.Println("Invalid column name:", columnName)
+		return
+	}
+
+	// Check if the field is exportable (i.e., if it starts with an uppercase letter)
+	if oldValueField.CanInterface() && newValueField.CanInterface() {
+		oldValue := oldValueField.Interface()
+		newValue := newValueField.Interface()
+
+		// Check if the types are comparable
+		if !reflect.TypeOf(oldValue).Comparable() || !reflect.TypeOf(newValue).Comparable() {
+			fmt.Println("Non-comparable field type:", columnName)
+			return
+		}
+
+		// Example condition to trigger an event if any change in the provided column
+		if !reflect.DeepEqual(newValue, oldValue) {
+			p.TriggerEvent = true
+		} else {
+			p.TriggerEvent = false
+		}
+	} else {
+		fmt.Println("Non-exported field:", columnName)
+	}
 }
 
 // afterUpdate is the callback function to be executed after an update operation
 func (p *SQSPlugin) afterUpdate(db *gorm.DB) {
 	fmt.Println("After update callback triggered. Pushing message to SQS...")
-	_, err := p.SQSClient.SendMessage(&sqs.SendMessageInput{
-		MessageBody: p.SQSMessage,
-		QueueUrl:    p.QueueURI,
-	})
-	if err != nil {
-		fmt.Println("Failed to publish message to SQS:", err)
-		return
+
+	// Debugging: Print the SQL query before execution
+	_ = db.Debug()
+
+	if p.TriggerEvent {
+		_, err := p.SQSClient.SendMessage(&sqs.SendMessageInput{
+			MessageBody: p.SQSMessage,
+			QueueUrl:    p.QueueURI,
+		})
+		if err != nil {
+			fmt.Println("Failed to publish message to SQS:", err)
+			return
+		}
+		fmt.Println("Message successfully published to SQS.")
 	}
-	fmt.Println("Message successfully published to SQS.")
-	fmt.Println(db.Statement.SQL.String())
 }
