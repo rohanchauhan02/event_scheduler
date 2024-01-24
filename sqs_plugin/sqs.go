eaa30b1637ab18edd1b14d95f75a0601651a6897
rohanchauhan02 <rohan021195@gmail.com>
2024-01-24 17:51:53 +0530

    initial sqs plugin commit

---
+++ sqs_plugin/sqs.go
@@ -0,0 +1,39 @@
+package sqsplugin
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// SQSPlugin is a GORM plugin for triggering SQS messages on updates
+type SQSPlugin struct{}
+
+// Name returns the name of the plugin
+func (p *SQSPlugin) Name() string {
+	return "SQSPlugin"
+}
+
+// Initialize initializes the plugin with the GORM DB instance
+func (p *SQSPlugin) Initialize(db *gorm.DB) error {
+	// Register the plugin's callback
+	db.Callback().Update().After("gorm:after_update").Register("sqs_plugin:after_update", p.afterUpdate)
+	return nil
+}
+
+// afterUpdate is the callback function to be executed after an update operation
+func (p *SQSPlugin) afterUpdate(db *gorm.DB) {
+	fmt.Println("After update callback triggered. Pushing message to SQS...")
+
+	// Perform your SQS message pushing logic here
+	// Example: pushMessageToSQS("Update operation detected!")
+
+	fmt.Println("Message pushed to SQS successfully.")
+}
+
+type CustomModel struct {
+	ID        uint `gorm:"primarykey"`
+	Name      string
+	UpdatedAt time.Time
+}
